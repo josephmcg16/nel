b818ee6c77f1a34d60f6822bfde7428ef5496f43
Joseph Mcgovern <81266818+josephmcg16@users.noreply.github.com>
2022-06-29 17:00:29 +0100

    Add density meter calc

--- density.py
+++ density.py
@@ -23,3 +23,25 @@ class Densitometer:
         self.coeffecients = np.array([
             K0, K1, K2, K18, K19, K20A, K20B, K21A, K21B
         ])
+
+    def calc_density(self, K_arr: np.array):
+        """Calculate densitometer density measurement from calibration coeffecients,
+        temperature and pressure readings"""
+        tau = self.data['tden']     # oscillation period [micro sec]
+        t = self.data['tfluid']     # temperature [degC]
+        p = self.data['pfluid']     # pressure [bara]
+
+        T_REF = 20  # reference temperature [degC]
+        P_REF = 1  # reference pressure [bara]
+
+        # density at reference temperature [kg/ m3]
+        rho_0 = K_arr[0] + K_arr[1] * tau + K_arr[2] * tau ** 2
+
+        # density corrected for temperature [kg/ m3]
+        rho_t = rho_0 * (1 + K_arr[3] * (t - T_REF)) + K_arr[4] * (t-T_REF)
+
+        # density corrected for temperature and pressure [kg/ m3]
+        rho_tp = rho_t * (1 + K_arr[5] * (p - P_REF) + K_arr[6] * (p - P_REF) ** 2) + \
+            (K_arr[7] * (p - P_REF) + K_arr[8] * (p - P_REF) ** 2)
+
+        return rho_tp.to_numpy()
