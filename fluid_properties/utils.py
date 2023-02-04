0a01158d77ac2d77cedc7e8331d34762e2c5cebf
josephmcg16 <joseph.mcgovern16@googlemail.com>
2023-02-04 18:35:08 +0000

    Add sat curve filter and code re-stucture.

--- fluid_properties/utils.py
+++ fluid_properties/utils.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import numpy as np
 from scipy import optimize
+from scipy.spatial import KDTree
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
+
+from tqdm import tqdm
+
 
 GAS_CONSTANT_DICT = {
     "CO2": 188.9,
@@ -29,16 +33,55 @@ def scrape_nist_isotherm(fluid, isotherm, pressure_range):
     return df
 
 
-def scrape_nist_data(fluid, temperature_range, pressure_range, vapor_phase=True):
+def scrape_nist_satcurve(fluid, pressure_range):
+    pressure_inc = (pressure_range[-1] - pressure_range[0]) / (len(pressure_range) - 1)
+    df = pd.read_html(f"https://webbook.nist.gov/cgi/fluid.cgi?PLow={pressure_range[0]}&PHigh={pressure_range[-1]}&PInc={pressure_inc}&Digits=5&ID={NIST_ID_DICT[fluid]}&Action=Load&Type=SatT&TUnit=C&PUnit=bar&DUnit=kg%2Fm3&HUnit=kJ%2Fkg&WUnit=m%2Fs&VisUnit=cP&STUnit=N%2Fm&RefState=DEF#Vapor")[0]
+    return df
+
+
+def filter_near_sat_curve(df, df_sat, n_samples):
+    # df indices now range from 0 to number of rows in df
+    df = df.reset_index(drop=True)
+
+    # create a KDTree from the saturation curve
+    sat_tree = KDTree(df_sat[['Pressure (bar)', 'Temperature (C)']])
+
+    # query the KDTree for the nearest neighbors of each sample in pT_data
+    _, closest_indices = sat_tree.query(
+        df[['Pressure (bar)', 'Temperature (C)']], k=1)
+
+    # find the distances between the samples and their nearest neighbors in the curve
+    distances = np.linalg.norm(df[['Pressure (bar)', 'Temperature (C)']].to_numpy(
+    ) - df_sat.iloc[closest_indices][['Pressure (bar)', 'Temperature (C)']].to_numpy(), axis=1)
+
+    # sort the distances in ascending order and select the n_samples closest samples based on their distances
+    closest_samples = np.argsort(distances)[:n_samples]
+    return df.drop(df.index[closest_samples], inplace=False)
+
+
+def scrape_nist_data(fluid, temperature_range, pressure_range, sat_curve_filter=0, vapor_phase=True, callback=False):
     df = pd.DataFrame()
-    for isotherm in temperature_range:
-        df = df.append(scrape_nist_isotherm(fluid, isotherm, pressure_range))
+
+    if callback:
+        for isotherm in tqdm(temperature_range):
+            df = df.append(scrape_nist_isotherm(fluid, isotherm, pressure_range))
+
+    else:
+        for isotherm in temperature_range:
+            df = df.append(scrape_nist_isotherm(fluid, isotherm, pressure_range))
 
     # normalized pressure and temperature
     df['pi'] = df['Pressure (bar)'] / 50
     df['tau'] = 300 / (df['Temperature (C)'] + 273.15)
 
+    if sat_curve_filter > 0:
+        df_sat = scrape_nist_satcurve(fluid, pressure_range)
+        filter_near_sat_curve(df, df_sat, sat_curve_filter)
+
     if vapor_phase:
-        return df[df['Phase'] == "vapor"]
-    else:
-        return df
+        df = df[df['Phase'] == "vapor"]
+
+
+    # df indices now range from 0 to number of rows
+    df = df.reset_index(drop=True)
+    return df
